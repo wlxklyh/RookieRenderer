805deaee4387442133f54348a14682b2749949f2
linyanhou <linyanhou@qq.com>
2021-10-16 02:32:01 +0800

    提交
    提交

--- Proj/src/Main/Main.cpp
+++ Proj/src/Main/Main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <fstream>
 #include "MathUtil.h"
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << MathUtil::HMin(3,12);
+    std::ofstream out;
+    out.open("OutputPic.ppm");
+
+    int PicW = 200;
+    int PicH = 200;
+
+    out << "P3\n" << PicW << " " << PicH << "\n255\n";
+    for (int i = 0; i < PicW; i++)
+    {
+        for (int j =0; j < PicH; j++)
+        {
+            float r = float(i) / float(PicW);
+            float g = float(j) / float(PicH);
+            float b = 0.2;
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+            out << ir << " " << ig << " " << ib << "\n";
+        }
+    }
 
     return 0;
 }
