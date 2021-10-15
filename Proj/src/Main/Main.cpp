d13cd69151d5da783514154e97152ff111c05aa2
wlxklyh <linyanhou@qq.com>
2021-10-16 02:51:59 +0800

    【Chapter1】用ppm格式的图片来显示结果

--- Proj/src/Main/Main.cpp
+++ Proj/src/Main/Main.cpp
@@ -6,9 +6,11 @@ int main()
     std::ofstream out;
     out.open("OutputPic.ppm");
 
+    //（1）宽高
     int PicW = 200;
     int PicH = 200;
 
+    // （2）这个是ppm图片格式 后面渲染的结果用这个来显示
     out << "P3\n" << PicW << " " << PicH << "\n255\n";
     for (int i = 0; i < PicW; i++)
     {
@@ -16,7 +18,7 @@ int main()
         {
             float r = float(i) / float(PicW);
             float g = float(j) / float(PicH);
-            float b = 0.2;
+            float b = 0.1;
             int ir = int(255.99 * r);
             int ig = int(255.99 * g);
             int ib = int(255.99 * b);
